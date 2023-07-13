a880c1d13259a25f6d48bfe4d38c49177efceb83
Dmitriy Sidorov <dmitriywhite@icloud.com>
2023-07-13 14:32:40 +0300

    event emitter

---
+++ src/shared/EventEmitter.ts
@@ -0,0 +1,33 @@
+type EventHandler = (data: any) => void;
+
+class EventEmitter {
+  private eventHandlers: { [eventName: string]: EventHandler[] } = {};
+
+  public on(eventName: string, handler: EventHandler): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+
+    this.eventHandlers[eventName]!.push(handler);
+  }
+
+  public off(eventName: string, handler: EventHandler): void {
+    const handlers = this.eventHandlers[eventName];
+
+    if (handlers) {
+      this.eventHandlers[eventName] = handlers.filter(
+        EventHandler => EventHandler !== handler,
+      );
+    }
+  }
+
+  public emit(eventName: string, data: any): void {
+    const handlers = this.eventHandlers[eventName];
+
+    if (handlers) {
+      handlers.forEach(handler => handler(data));
+    }
+  }
+}
+
+export const eventEmitter = new EventEmitter();
